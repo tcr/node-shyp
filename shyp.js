4b36b29bc3450311e8973a854a110b9616dab3a1
Tim Cameron Ryan <id@timryan.org>
2014-01-15 15:26:09 -0500

    v0.1.5 Package dlls

--- shyp.js
+++ shyp.js
@@ -50,7 +50,7 @@ if (process.argv[2] == 'publish') {
 			var outdirabi = outdir + abi + '/';
 			mkdirp(outdirabi, function (err) {
 				fs.readdirSync(builddir).forEach(function (file) {
-					if (fs.lstatSync(builddir + file).isFile() && file.match(/\.(exp|lib|node|dylib|exe)$|^[^.]+$/)) {
+					if (fs.lstatSync(builddir + file).isFile() && file.match(/\.(exp|lib|node|dylib|exe|dll)$|^[^.]+$/)) {
 						fs.writeFileSync(outdirabi + file, fs.readFileSync(builddir + file), {
 							mode: fs.lstatSync(builddir + file).mode
 						});
